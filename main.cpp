7a7c8bf5fc2f77022a738b27dff26a5be82b0570
RoliSoft <rolisoft@gmail.com>
2015-12-29 22:49:18 +0200

    Reorganized test files.

--- main.cpp
+++ main.cpp
@@ -78,11 +78,11 @@ void show_image(const Mat& img, const string& modifier = "", bool histogram = tr
  */
 void test_lsb()
 {
-	auto img = imread("img_small.png");
+	auto img = imread("test/img_small.png");
 
 	show_image(img, "Original");
 
-	auto input  = read_file("test.txt");
+	auto input  = read_file("test/test.txt");
 	auto stego  = encode_lsb(img, encode_tlv(input));
 	auto output = decode_tlv(decode_lsb(stego));
 
@@ -96,11 +96,11 @@ void test_lsb()
  */
 void test_lsb_alt()
 {
-	auto img = imread("img_small.png");
+	auto img = imread("test/img_small.png");
 
 	show_image(img, "Original");
 
-	auto input  = read_file("test.txt");
+	auto input  = read_file("test/test.txt");
 	auto stego  = encode_lsb_alt(img, encode_tlv(input));
 	auto output = decode_tlv(decode_lsb_alt(stego));
 
@@ -114,11 +114,11 @@ void test_lsb_alt()
  */
 void test_dct()
 {
-	auto img = imread("lena.jpg");
+	auto img = imread("test/lena.jpg");
 
 	show_image(img, "Original");
 
-	auto input  = read_file("test.txt");
+	auto input  = read_file("test/test.txt");
 	auto stego  = encode_dct(img, input);
 	auto output = decode_dct(stego);
 
@@ -133,17 +133,17 @@ void test_dct()
  */
 void test_dct_multi()
 {
-	auto img = imread("lena.jpg");
+	auto img = imread("test/lena.jpg");
 
 	show_image(img, "Original");
 
-	auto input = read_file("test.txt");
+	auto input = read_file("test/test.txt");
 	auto stego = encode_dct(img,   input, STORE_FULL, 0);
 	     stego = encode_dct(stego, input, STORE_FULL, 1);
 		 stego = encode_dct(stego, input, STORE_FULL, 2);
 
-	imwrite("lena_dct.jpg", stego, vector<int> { CV_IMWRITE_JPEG_QUALITY, 80 });
-	stego = imread("lena_dct.jpg");
+	imwrite("test/lena_dct.jpg", stego, vector<int> { CV_IMWRITE_JPEG_QUALITY, 80 });
+	stego = imread("test/lena_dct.jpg");
 
 	auto output = repair(vector<string>
 		{
@@ -162,11 +162,11 @@ void test_dct_multi()
  */
 void test_dwt()
 {
-	auto img = imread("lena.jpg");
+	auto img = imread("test/lena.jpg");
 
 	show_image(img, "Original");
 
-	auto input  = read_file("test.txt");
+	auto input  = read_file("test/test.txt");
 	auto stego  = encode_dwt(img, input);
 	auto output = decode_dwt(img, stego);
 
@@ -181,17 +181,17 @@ void test_dwt()
  */
 void test_dwt_multi()
 {
-	auto img = imread("lena.jpg");
+	auto img = imread("test/lena.jpg");
 
 	show_image(img, "Original");
 
-	auto input = read_file("test.txt");
+	auto input = read_file("test/test.txt");
 	auto stego = encode_dwt(img,   input, STORE_FULL, 0);
 	     stego = encode_dwt(stego, input, STORE_FULL, 1);
 		 stego = encode_dwt(stego, input, STORE_FULL, 2);
 
-	imwrite("lena_dwt.jpg", stego, vector<int> { CV_IMWRITE_JPEG_QUALITY, 90 });
-	stego = imread("lena_dwt.jpg");
+	imwrite("test/lena_dwt.jpg", stego, vector<int> { CV_IMWRITE_JPEG_QUALITY, 90 });
+	stego = imread("test/lena_dwt.jpg");
 
 	auto output = repair(vector<string>
 		{
@@ -222,7 +222,7 @@ int main(int argc, char** argv)
 	//test_dct();
 	test_dct_multi();
 	//test_dwt();
-	//test_dwt_multi();
+	test_dwt_multi();
 
 	cvWaitKey();
 
